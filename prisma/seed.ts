3a93975b05caa2abff7e00613365789e190927c5
Yongjun <wangyj641@gmail.com>
2024-02-28 21:51:28 +0800

    Frontend get data from DB

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+
+import example from "./example.json";
 // 初始化 Prisma Client
 const prisma = new PrismaClient();
-
-async function main() {
-  //在此编写 Prisma Client 查询
-}
 
 main()
   .catch((e) => {
@@ -15,3 +13,52 @@ main()
     // 关闭 Prisma Client
     await prisma.$disconnect();
   });
+
+// Insert data to table: user
+// async function main() {
+//   const user = await prisma.user.create({
+//     data: {
+//       name: "wangyj641",
+//       avatar:
+//         "https://p3-passport.byteimg.com/img/user-avatar/585e1491713363bc8f67d06c485e8260~100x100.awebp",
+//     },
+//   });
+//   console.log(user);
+// }
+
+// Insert data to tables: category, chapter and video
+async function main() {
+  const category = await prisma.category.create({
+    data: {
+      name: "数学",
+    },
+  });
+
+  const chapters = example.data.outlines.reduce((res: any[], item) => {
+    item.lectures.forEach((lecture) => {
+      res.push({
+        title: lecture.title ?? lecture.en_title ?? "",
+        cover: lecture.resource.cover_url,
+        url: lecture.resource.content[0].url,
+      });
+    });
+
+    return res;
+  }, []);
+  console.log(chapters);
+
+  await prisma.video.create({
+    data: {
+      title: example.data.title,
+      pic: example.data.cover_url,
+      desc: example.data.brief,
+      categoryId: category.id,
+      authorId: 1,
+      chapter: {
+        createMany: {
+          data: chapters,
+        },
+      },
+    },
+  });
+}
